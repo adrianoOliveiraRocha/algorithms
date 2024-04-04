c7f9991fa2ee54356a86b2fdb002d620cabd7058
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2024-04-04 08:35:52 -0300

    go

--- problems/balanced/balanced.js
+++ problems/balanced/balanced.js
@@ -3,13 +3,25 @@
 function balanced(centrifuge) {
   console.log(centrifuge, "\n")
   let chunk = null, compare = null;
+  let chunkInit = 0, compareInit = 1;
+  let res = false; 
   for(let i = 1; i < centrifuge.length/2+1; i++) {
-    chunk = centrifuge.slice(0, i);
-    compare = centrifuge.slice(i, i+chunk.length);
-    // console.log("chunk: ", chunk, "i: ", i);
-    console.log("chunk: ", chunk, "compare: ", compare);
+    chunk = centrifuge.slice(chunkInit, i);
+    compare = centrifuge.slice(chunk.length, i+chunk.length);
+    
+    if(chunk.join('') == compare.join('')) {
+      console.log('equals', chunk, compare, 'i+chunk.length: ', i+chunk.length);
+      test(chunk, centrifuge.slice(i+chunk.length, centrifuge.length));
+    } else {
+      console.log("difrrent: ", chunk, compare);      
+    }    
   }
-  return false;
+  function test(chunk, arr) {
+    console.log("Test chunk with the rest of the array");
+    console.log(chunk, arr);
+    return;
+  }
+  return res;
 }
 
 module.exports = balanced;
