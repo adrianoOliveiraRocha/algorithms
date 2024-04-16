3a1f4cbd49e0ba32e945064d9011c919b259db28
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2024-04-16 07:13:09 -0300

    #

--- problems/possibleScores/main.js
+++ problems/possibleScores/main.js
@@ -1,7 +1,25 @@
 'use strict'
 
 function possibleScores(answerOfTom, scoreOfTom, answerOfJohn) {
-
+  
+  if(answerOfTom == answerOfJohn) { //equals
+    return [scoreOfTom, scoreOfTom]
+  } else {
+    let dif = 0, min, max;
+    for(let i = 0; i < answerOfJohn.length; i++) {
+      if(answerOfJohn[i] != answerOfTom[i]) {
+        dif++;
+      }
+    }
+    
+    if(dif == 10) { //oposite
+      return [100-scoreOfTom, 100-scoreOfTom];
+    } else {
+      min = scoreOfTom - (dif * 10);
+      max = scoreOfTom + (dif * 10);
+      return [min, max];
+    }    
+  }    
 }
 
 module.exports = possibleScores;
--- problems/possibleScores/tests.js
+++ problems/possibleScores/tests.js
@@ -1,23 +1,52 @@
 'use struct'
-//<minimum possible score of John>, <maximum possible score of John>
+/*
+"xxxooxoooo",70,"xxxoxxoooo"),[60,80]
+"ooxooxooxo",50,"oooooxxxxx"),[10,90]
+"xxxooxoooo",70,"xxxoxxoooo"),[60,80]
+*/
+const possibleScores = require('./main.js');
 const Test = {
   test1() {
     let answerOfTom = "oooooooooo";
     let scoreOfTom = 20;
-    let answerOfJohn = "oooooooooo"; //20,20
+    let answerOfJohn = "oooooooooo"; 
+    console.log(possibleScores(answerOfTom, scoreOfTom, answerOfJohn)) //20,20
   },
 
   test2() {
     let answerOfTom = "oooooooooo";
     let scoreOfTom = 20;
-    let answerOfJohn = "xxxxxxxxxx"; //80,80
+    let answerOfJohn = "xxxxxxxxxx"; 
+    console.log(possibleScores(answerOfTom, scoreOfTom, answerOfJohn)) //80,80
   },
 
   test3() {
     let answerOfTom = "oooooooooo";
     let scoreOfTom = 20;
-    let answerOfJohn = "oooooxxxxx"; //30,70
+    let answerOfJohn = "oooooxxxxx"; 
+    console.log(possibleScores(answerOfTom, scoreOfTom, answerOfJohn)) //30,70
+  },
+
+  test4() {
+    "x x x o o x o o o o" // 70
+    "x x x o x x o o o o" // [60,80]
+    // mandou uma diferente. O mínimo é 7 - 1 (uma diferente)
+    let answerOfTom = "xxxooxoooo";
+    let scoreOfTom = 70;
+    let answerOfJohn = "xxxoxxoooo"; 
+    console.log(possibleScores(answerOfTom, scoreOfTom, answerOfJohn)) //[60,80]
+  },
+
+  test5() {
+    "o o x o o x o o x o" //50 
+    "o o o o o x x x x x" //[10,90]
+    // mandou quatro diferentes. O mínimo é 5 - 4 (quatro diferentes)
+    let answerOfTom = "ooxooxooxo";
+    let scoreOfTom = 50;
+    let answerOfJohn = "oooooxxxxx"; 
+    console.log(possibleScores(answerOfTom, scoreOfTom, answerOfJohn)) //[10,90]
   }
 }
+Test.test4(); 
+Test.test5();
 
-Test.test1();
