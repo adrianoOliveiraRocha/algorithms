ef05291de1aa174dfabbfbb12d37d9df30ee59a4
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2024-05-04 07:22:36 -0300

    #

--- problems/generateHashtag/generateHashtag.js
+++ problems/generateHashtag/generateHashtag.js
@@ -1,6 +1,6 @@
 'use strict'
-
-function generateHashtag (str) {  
+// Passou. Vai pŕo próximo
+function generateHashtag (str) { 
   function onlySpace(str) {
     let isSpace = 0;
     for(let i = 0; i < str.length; i++) {      
@@ -9,17 +9,21 @@ function generateHashtag (str) {
     if(str.length == isSpace) return true;
   }
 
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   if(str.length == 0) return false;
-  if(str.length > 140) return false;
   if(onlySpace(str)) return false;
 
   let words = str.split(' ');
-  let newStr;
+  let newStr = "#";
   for(let i = 0; i < words.length; i++) {
-    newStr+="#";
-    newStr+=words[i].toUpperCase();
+    newStr += capitalizeFirstLetter(words[i]);
   }
-  return newStr;
+
+  if(newStr.length > 140) return false;
+  else return newStr;
 }
 
 module.exports = generateHashtag;
--- problems/generateHashtag/test.js
+++ problems/generateHashtag/test.js
@@ -13,6 +13,23 @@ const Test = {
   test3() { // more than 140 characters
     console.log(generateHashtag("Do We have A Hashtag")); // #DoWeHaveAHashtag
   },
+
+  test4() {
+    console.log(generateHashtag("Codewars"));
+  },
+
+  test5() {
+    console.log(generateHashtag("Codewars Is Nice"));
+  },
+
+  test6() {
+    console.log(generateHashtag("code" + " ".repeat(140) + "wars"))
+  }
 }
 
+Test.test1();
+Test.test2();
 Test.test3();
+Test.test4();
+Test.test5();
+Test.test6();
