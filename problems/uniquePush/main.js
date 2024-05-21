5fa37e1d1d7d2ecc1e9a15e3d5aa0fff5ab78655
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2024-05-21 15:11:50 -0300

    #

--- problems/uniquePush/main.js
+++ problems/uniquePush/main.js
@@ -1,10 +1,17 @@
 'use strict'
 
+//Response received but no data was written to STDOUT or STDERR. 
+// Aí dentu
 function uniquePush(arr, obj) {
+  if(!obj.phoneNumber || obj.phoneNumber == undefined) {
+    return false;
+  }
+  // console.log(arr)
   let length = arr.length;
   let unique = true;
   for(let i = 0; i < length; i++) {
-    if(arr[i].name == obj.name && arr[i].phoneNumber == obj.phoneNumber) {
+    
+    if(arr[i].phoneNumber == obj.phoneNumber) {
       unique = false;
       break;
     }
@@ -12,6 +19,7 @@ function uniquePush(arr, obj) {
   
   if(unique) {
     arr.push(obj);
+    // console.log(arr)
     return true;
   } else {
     return false;
--- problems/uniquePush/test.js
+++ problems/uniquePush/test.js
@@ -1,15 +1,36 @@
 'use strict'
 const uniquePush = require('./main');
 const Test = {
-  test1() {
+  test1() { // it is unique
     let arr = [
       {name:'bob', phoneNumber:1234},
-      {name:'joe', phoneNumber:12345},
-      {name:'I am duped', phoneNumber:12345}
+      {name:'joe', phoneNumber:12345}
+    ]
+    let obj = {name:'July', phoneNumber:87654555};
+    console.log(uniquePush(arr, obj)); //true
+  },
+
+  test2() { // it isn't unique
+    let arr = [
+      {name:'bob', phoneNumber:1234},
+      {name:'joe', phoneNumber:12345}
     ]
     let obj = {name:'July', phoneNumber:12345};
-    console.log(uniquePush(arr, obj));
+    console.log(uniquePush(arr, obj)); //false
+  },
+
+  test3() { // it doesn't has a phone number
+    let arr = [
+      {name:'bob', phoneNumber:1234},
+      {name:'joe', phoneNumber:12345}
+    ]
+    let obj = {name:'July', phoneNumber:undefined};
+    console.log(uniquePush(arr, obj)); //false
+    obj = {name:'July'};
+    console.log(uniquePush(arr, obj)); //false
   }
 }
 
 Test.test1()
+Test.test2()
+Test.test3()
