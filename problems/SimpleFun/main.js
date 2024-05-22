ada103480c70bfebef96c0ee5626eef861102ef1
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2024-05-22 08:18:56 -0300

    #

--- problems/SimpleFun/main.js
+++ problems/SimpleFun/main.js
@@ -1,6 +1,29 @@
-
+'use strict'
+/*
+Keep the sumSquares in an array. When apper a repetition, count the size 
+of the array from repeated number to the last number without count with 
+repetition
+*/
 function repeatSequenceLen(a0) {
-  //coding and coding.. 
+  let sequence = [];
+  let nArr = a0.toString().split("");
+  let sumSquare = ((nArr) => {
+    let r = 0;
+    for(let i = 0; i < nArr.length; i++) {
+      let x = Math.pow(parseInt(nArr[i]), 2)
+      r += x
+    }
+    return r
+  })(nArr)
+  
+  if(!sequence.includes(sumSquare)) {
+    sequence.push(sumSquare)
+    console.log(sequence)
+    repeatSequenceLen(sumSquare)
+  }
+  else {
+    return sequence;
+  }  
 }
 
 module.exports = repeatSequenceLen
--- problems/SimpleFun/test.js
+++ problems/SimpleFun/test.js
@@ -1,4 +1,11 @@
+'use strict'
+
+const repeatSequenceLen = require('./main')
 
 const Test = {
-  
+  test1() {
+    console.log(repeatSequenceLen(85));
+  }
 }
+
+Test.test1();
