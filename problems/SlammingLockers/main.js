b356404498dc0bfb12ede540c24043fd2caa4560
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2024-06-04 08:09:43 -0300

    #

--- problems/SlammingLockers/main.js
+++ problems/SlammingLockers/main.js
@@ -1,12 +1,22 @@
-/*
-i = 0: open all
-i = 1: jump each 1. close wich is openned, open wich is openned
-i = 2: jump each 2. close wich is openned, open wich is openned
-*/
-
+// Error: Execution Timed Out (12000 ms)
 function lockerRun(lockers){
-  let openned = Array.from({length: lockers}, (_, i) => i + 1)
-  return openned
+  let lObj = {}, openned = []  
+  let jump = 1
+  while(jump <= lockers) {
+    for(let i = jump; i <= lockers; i += jump) {
+      if(jump == 1) lObj[i] = true
+      else {
+        if(lObj[i] == true) lObj[i] = false
+        else lObj[i] = true
+      }      
+    }
+    jump++
+  }
+
+  Object.keys(lObj).forEach(key => {
+    if(lObj[key]) openned.push(parseInt(key))
+  })  
+  return lObj
 }
 
 module.exports = lockerRun
--- problems/SlammingLockers/test.js
+++ problems/SlammingLockers/test.js
@@ -2,8 +2,28 @@ const lockerRun = require('./main')
 
 const Test = {
   test1() {
-    console.log(lockerRun(4))
+    console.log(lockerRun(1))
+  },
+
+  test2() {
+    console.log(lockerRun(2))
+  },
+
+  test3() {
+    console.log(lockerRun(3))
+  },
+
+  test5() {
+    console.log(lockerRun(5))
+  },
+
+  test8() {
+    console.log(lockerRun(8))
   }
 }
 
-Test.test1();
+// Test.test1();
+// Test.test2();
+// Test.test3();
+// Test.test5();
+Test.test8();
