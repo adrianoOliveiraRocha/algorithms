dda2dff11cfd4351c3557669f4907e44e4a7005d
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2024-06-05 08:22:53 -0300

    #

--- problems/SlammingLockers/main.js
+++ problems/SlammingLockers/main.js
@@ -1,23 +1,29 @@
-// Error: Execution Timed Out (12000 ms)
+/*
+Why did my code time out?
+Our servers are configured to only allow a certain amount of time 
+for your code to execute. In rare cases the server may be taking on 
+too much work and simply wasn't able to run your code efficiently 
+enough. Most of the time though this issue is caused by inefficient 
+algorithms. If you see this error multiple times you should try to 
+optimize your code further.
+*/
 function lockerRun(lockers){
-  let lObj = {}, openned = []  
+  let openned = []  
   let jump = 1
   while(jump <= lockers) {
     for(let i = jump; i <= lockers; i += jump) {
       if(jump == 1) {
-        lObj[i] = true
         openned.push(i)
       } else {
-        if(lObj[i] == true) {
-          lObj[i] = false
-          removeItemOnce(i);
-        }
-        else {
-          lObj[i] = true
+        if(openned.includes(i)) {
+          removeItemOnce(i)
+        } else {
           openned.push(i)
         }
       }      
     }
+    console.log("\n")
+    console.log(i, openned)
     jump++
   }
 
