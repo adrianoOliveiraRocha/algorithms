7128d31d158aa2312ae2050027debf7b00ca2044
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2024-06-24 07:25:43 -0300

    #

--- problems/PitchClassSet/main.js
+++ problems/PitchClassSet/main.js
@@ -1,18 +1,20 @@
 'use strict'
+//ok. next..
+
 
 /*
-1 - transposition(T) is simply to add the same number (between 0 and 11) 
+1 - transposition(T) is simply to add the same number (between 0 and 11)
   to all elements of the collection.
   A T5 of the set [1, 2, 3] should be [6, 7, 8]
 
-2 - To operate an inversion(I) subtract each element of the list from twelve 
-  Note that if one asks for the TnI inversion of some set it means the 
-  inversion of the transposition n. Consequently, the TnI of [1,2,3] 
+2 - To operate an inversion(I) subtract each element of the list from twelve
+  Note that if one asks for the TnI inversion of some set it means the
+  inversion of the transposition n. Consequently, the TnI of [1,2,3]
   should be [(12-1)+n, (12-2)+n, (12-3)+n] <=> [0,11,10].
 
-You should create a function that receives a pitch class set (array) 
-as first argument and a transposition or inversion operation (string) 
-as second argument, formated as "Tn" (transposition n) or "TnI" 
+You should create a function that receives a pitch class set (array)
+as first argument and a transposition or inversion operation (string)
+as second argument, formated as "Tn" (transposition n) or "TnI"
 (inversion n) where n is a number between 0 and 12. Below are some examples:
 
 operation([4,6,7,10], "T1");
@@ -30,16 +32,22 @@ result = [0,3,4] //sorted
 
 function operate(pcSet, operation) {
   let result = [];
-  // console.log(pcSet)
   if(operation.includes('I')) { // inversion
-    
+    let n = parseInt(operation.slice(1, operation.length))
+    pcSet.forEach(el => {
+      let v = (12 - el) + n
+      if(v <= 11) result.push((12 - el) + n)
+      else result.push(v % 12)
+    });
+
   } else { // transposition
     let n = parseInt(operation.slice(1))
     pcSet.forEach(el => {
-      if(el+n < 11) result.push(el+n);
-      else result.push((el+11) - 13);
+      let v = el + n
+      if(v <= 11) result.push(v);
+      else result.push(v % 12);
     });
-  }  
+  }
   return result.sort((a, b) => a-b);
 }
 
--- problems/PitchClassSet/test.js
+++ problems/PitchClassSet/test.js
@@ -2,22 +2,29 @@
 const operate = require('./main')
 
 const Tests = {
-  test1() {
-    console.log(operate([4,6,7,10], "T1")) //[5,7,8,11]
+  test1() { //ok
+    console.log("test1(): ", operate([4,6,7,10], "T1")) //[5,7,8,11]
   },
 
-  test2() {
-    console.log(operate([4,6,7,10], "T1I")) //[3,6,7,9] //sorted
+  test2() { //ok
+    console.log("test2(): ", operate([10,6,7], "T10")) //[4,5,8] //sorted
   },
 
-  test3() {
-    console.log(operate([10,6,7], "T10")) //[4,5,8] //sorted
+  test3() { //ok
+    console.log("test3(): ", operate([4,6,7,10], "T1I")) //[3,6,7,9] //sorted
   },
 
   test4() {
-    console.log(operate([10,6,7], "T10I")) //[0,3,4] //sorted
+    console.log("test4(): ", operate([10,6,7], "T10I")) //[0,3,4] //sorted
   },
+
+  test5() {
+    console.log("test4(): ", operate([1,2,3], "T11")) //[0,1,2]
+  }
 }
 
 Tests.test1();
+Tests.test2();
 Tests.test3();
+Tests.test4();
+Tests.test5()
