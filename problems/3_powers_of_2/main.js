e1e4c7c9551f8eb2743f5f8311a5e10e152d6e46
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2024-07-16 08:27:09 -0300

    #

--- problems/3_powers_of_2/main.js
+++ problems/3_powers_of_2/main.js
@@ -1,35 +1,30 @@
 'use strict'
-
+/*
+1 - numa divisão por 3 o resto pode ser 1 ou 2
+2 - se o resultado de n / 3 for uma potência de 2, soma os dois 
+primeiros números e coloca o resto na segunda posição
+*/
 function threePowers(n) {
+  if(n < 3) return false;
+  n = parseInt(n)
   let r = n % 3
-  let v = new Array(3).fill(parseInt(n/3))
-   
-  while(r > 0) {
-    for(let i = 0; i < v.length; i++) {
-      v[i] += 1;
-      r -= 1;
-      if(r == 0) break;
-    }
+  let v = parseInt(n/3)
+    
+  if(testpw(v)) {
+    return true;
+  } else {
+    return false
   }
   
-  let testR = true
-  for(let i = 0; i < v.length; i++) {
-    if(!testpw(v[i])) {
-      testR = false
-      break;
-    }
-  }  
-  
-  return [v, testR]
 }
 
 function testpw(v) {
-    let x = v;
-    while(x > 2) {
-      if(x % 2 != 0) return false;
-      x = x/2;
-    }
-    return true
+  let x = v;
+  while(x > 2) {
+    if(x % 2 != 0) return false;
+    x = x/2;
   }
+  return true
+}
 
 module.exports = threePowers;
--- problems/3_powers_of_2/test.js
+++ problems/3_powers_of_2/test.js
@@ -1,11 +1,39 @@
 'use strict'
 const threePowers = require('./main.js')
-
+/*
+n = 9: expected false to equal true
+n = 276: expected false to equal true
+*/
 const Tests = {
   test1() {
-    console.log(threePowers(7))  
+    console.log(threePowers(2))  
+  },
+  
+  test2() {
+    console.log(threePowers(3))  
+  },
+  
+  test3() {
+    console.log(threePowers(5))  
+  },
+  
+  test4() {
+    console.log(threePowers(15))  
+  },
+  
+  test5() {
+    console.log(threePowers(9))  
+  },
+  
+  test6() {
+    console.log(threePowers(276))  
   }
 }
 
-Tests.test1();
+//Tests.test1();
+//Tests.test2();
+//Tests.test3();
+//Tests.test4();
+Tests.test5()
+Tests.test6()
 
