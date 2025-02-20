6452ddf30cedcb1b09c6efebebf9716da008f157
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2025-02-20 09:37:21 -0300

    go

--- problems/numTilePossibilities/main.js
+++ problems/numTilePossibilities/main.js
@@ -1,50 +1,44 @@
 'use strict'
 
 function numTilePossibilities(tiles) {
-  let diferents = Array.from(new Set([...tiles.split("")])).join("")
+  let diferents = Array.from(new Set([...tiles.split("")])).join("");
   let res = {}
-  res[diferents] = 1;
+  res[diferents] = permutation(diferents);
   
   for(let i = 0; i < tiles.length + 1; i++) {
     if(!Object.values(res).includes(tiles[i]) && tiles[i] != undefined)
       res[tiles[i]] = 1;
-    if(!Object.keys(res).includes(tiles.slice(0, i)) && tiles.slice(0, i).length > 0)
-      res[tiles.slice(0, i)] = 1;
+    if(!Object.keys(res).includes(tiles.slice(0, i)) && tiles.slice(0, i).length > 0) {
+      let el = tiles.slice(0, i);
+      res[el] = permutation(el);  
+    }      
   }
 
-  Object.keys(res).forEach(key => {
-    if(!key.split('').every((char, index, arr) => char === arr[0])) {
-      console.log("simple permutation of ", key)
+  function permutation(el) {
+    if(el.split('').every((char, index, arr) => char === arr[0])) {
+      return 1;
     } else {
-      console.log(key, res[key]);
-    }
-  });
-
-  
-  /*
-  function allCharactersEqual(str) {
-    return str.split('').every((char, index, arr) => char === arr[0]);
+      let denominator = el.length;
+      let denominatorFat = fatorial(denominator);
+      let numeratorFat = 1;
+      for(let i = 0; i < diferents.length; i++) {
+        let dif = diferents[i]
+        numeratorFat *= fatorial(el.split(dif).length - 1);
+      }
+      
+      return denominatorFat / numeratorFat;
+    } 
   }
-  if a element has more then one different character, simple permutation 
 
-  P(n;n1​,n2​,…,nk​)=n1​!⋅n2​!⋅⋯⋅nk​!n!​
+  function fatorial(n) {
+    let res = 1;
+    for(let i = 1; i <= n; i++) {
+      res*=i;
+    }
+    return res
+  }
 
-  Onde:
-
-  nn é o número total de elementos.
-
-  n1,n2,…,nkn1​,n2​,…,nk​ são as quantidades de elementos repetidos.
-  No caso da palavra AAB:
-
-    Temos n=3 caracteres no total.
-
-    A letra A repete-se 2 vezes.
-
-    A letra B aparece 1 vez.
-
-    P(3;2,1)= 3!​ / 2!⋅1!
-  */ 
-  return res;
+  return Object.values(res).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
 
 module.exports = numTilePossibilities;
