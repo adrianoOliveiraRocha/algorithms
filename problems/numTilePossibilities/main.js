2f60a736869d4c9404c3cafc556642bde96a9740
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2025-02-21 09:13:38 -0300

    #

--- problems/numTilePossibilities/main.js
+++ problems/numTilePossibilities/main.js
@@ -1,44 +1,32 @@
 'use strict'
 
 function numTilePossibilities(tiles) {
-  let diferents = Array.from(new Set([...tiles.split("")])).join("");
-  let res = {}
-  res[diferents] = permutation(diferents);
-  
-  for(let i = 0; i < tiles.length + 1; i++) {
-    if(!Object.values(res).includes(tiles[i]) && tiles[i] != undefined)
-      res[tiles[i]] = 1;
-    if(!Object.keys(res).includes(tiles.slice(0, i)) && tiles.slice(0, i).length > 0) {
-      let el = tiles.slice(0, i);
-      res[el] = permutation(el);  
-    }      
+  // Count the frequency of each letter
+  const count = {};
+  for (const tile of tiles) {
+    if(!count[tile]) 
+      count[tile] = 1;
+    else 
+      count[tile]++;
   }
-
-  function permutation(el) {
-    if(el.split('').every((char, index, arr) => char === arr[0])) {
-      return 1;
-    } else {
-      let denominator = el.length;
-      let denominatorFat = fatorial(denominator);
-      let numeratorFat = 1;
-      for(let i = 0; i < diferents.length; i++) {
-        let dif = diferents[i]
-        numeratorFat *= fatorial(el.split(dif).length - 1);
+  // Backtracking function to generate sequences
+  function backtrack() {
+    let total = 0;
+    for(const letter in count) {
+      if(count[letter] > 0) {
+        // Choose this letter
+        total++;
+        count[letter]--;
+        // Recurse with the updated count
+        total += backtrack();
+        // Unchoose this letter
+        count[letter]++;
       }
-      
-      return denominatorFat / numeratorFat;
-    } 
-  }
-
-  function fatorial(n) {
-    let res = 1;
-    for(let i = 1; i <= n; i++) {
-      res*=i;
     }
-    return res
+    return total;
   }
 
-  return Object.values(res).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  return backtrack();
 }
 
 module.exports = numTilePossibilities;
--- problems/numTilePossibilities/tests.js
+++ problems/numTilePossibilities/tests.js
@@ -7,7 +7,26 @@ const Tests = {
     // "BA", "ABA", "BAA".
     let tiles = "AAB";
     console.log(numTilePossibilities(tiles));
+  },
+
+  t2() {
+    let tiles = "AAABBC";
+    console.log(numTilePossibilities(tiles));
+  },
+
+  t3() {
+    let tiles = "V";
+    console.log(numTilePossibilities(tiles));
+  },
+
+  aleatories() {
+    let count = { A: 2, B: 1 };
+    for(const letter in count) {
+      console.log(letter);
+    }
   }
 }
-
-Tests.t1()
+Tests.t1() // 8
+// Tests.t2() // 188
+// Tests.t3() // 1
+// Tests.aleatories();
