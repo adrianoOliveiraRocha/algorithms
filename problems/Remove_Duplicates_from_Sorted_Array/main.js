9c303da533b2cad2791775fcdfe2891b9554b993
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2025-03-06 07:42:38 -0300

    #

--- problems/Remove_Duplicates_from_Sorted_Array/main.js
+++ problems/Remove_Duplicates_from_Sorted_Array/main.js
@@ -1,13 +1,8 @@
 'use strict'
 // next => https://leetcode.com/problems/remove-element/
 var removeDuplicates = function(nums) {
-  let current = nums[0], res = [nums[0]]; 
-  for(let i = 1; i < nums.length; i++) {
-    if(nums[i] != current) {
-      res.push(nums[i]);      
-    }
-    current = nums[i];
-  }
+  let newSet = new Set([...nums]);
+  let res = Array.from(newSet);
   return res;
 };
 
